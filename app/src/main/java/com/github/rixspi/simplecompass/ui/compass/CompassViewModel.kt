0a9cb22b147652c0b99fee03b65b79464d2fce4c
Ryszard Śpiewak <ryszard.spiewak@rst-it.com>
2018-02-06 20:19:25 +0100

    compass views and bindings

---
+++ app/src/main/java/com/github/rixspi/simplecompass/ui/compass/CompassViewModel.kt
@@ -0,0 +1,9 @@
+package com.github.rixspi.simplecompass.ui.compass
+
+import com.github.rixspi.simplecompass.ui.base.BaseViewModel
+import javax.inject.Inject
+
+
+class CompassViewModel @Inject constructor(): BaseViewModel() {
+
+}
